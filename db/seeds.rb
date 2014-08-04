5b9a555cc511402bc105e5323870831e8627f006
Alain Dutra <alain@drsolution.org>
2014-08-03 22:37:25 -0300

    changes to seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,5 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+User.destroy_all
 user = CreateAdminService.new.call
-puts 'CREATED ADMIN USER: ' << user.email
+puts "CREATED ADMIN USER: first_name: #{user.first_name}, last_name: #{user.last_name}, email: #{user.email}"
