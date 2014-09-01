c4701cc47c2848370e89334659209efa4443994e
Alain Dutra <alain@drsolution.org>
2014-09-01 11:58:39 -0300

    Added indexes to user_config model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140831160030) do
+ActiveRecord::Schema.define(version: 20140901115100) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -82,6 +82,8 @@ ActiveRecord::Schema.define(version: 20140831160030) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  add_index "user_configs", ["user_id"], name: "index_user_configs_on_user_id", using: :btree
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
