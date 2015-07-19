7b7ced0aec45cc14c48259581f10c1d23259d484
Alain Dutra <alain@drsolution.org>
2015-07-19 19:44:33 -0300

    changed env

--- connectMedic.php
+++ connectMedic.php
@@ -6,24 +6,24 @@
 
 // Dados do banco do heroku
 $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-$cleardb_server = $cleardb_url["host"];
-$cleardb_username = $cleardb_url["user"];
-$cleardb_password = $cleardb_url["pass"];
-$cleardb_db = substr($cleardb_url["path"], 1);
+global $cleardb_server = $cleardb_url["host"];
+global $cleardb_username = $cleardb_url["user"];
+global $cleardb_password = $cleardb_url["pass"];
+global $cleardb_db = substr($cleardb_url["path"], 1);
 
 // certificados
 
 
 // cert_pem
-$cert_pem = getenv("CERT_PEM");
+global $cert_pem = getenv("CERT_PEM");
 
 
 //cert_key
-$cert_key = getenv("CERT_KEY");
+global $cert_key = getenv("CERT_KEY");
 
 //cleardb_ca
 
-$cleardb_ca = getenv("CLEARDB_CA");
+global $cleardb_ca = getenv("CLEARDB_CA");
 
 
 
@@ -50,12 +50,14 @@ $tabelaUser = 'user_system';
 
 
 function connect() {
+
 	echo "<br><br>";
 echo "cleardb_server = $cleardb_server<br>";
 echo "us-cdbr-iron-east-02.cleardb.net<br>";
 echo "cleardb_username = $cleardb_username<br>";
 echo "cleardb_password = $cleardb_password<br>";
 echo "cleardb_db = $cleardb_db<br>";
+	
 	$con = mysqli_init();
 	mysqli_ssl_set($con, $cert_key, $cert_pem, $cleardb_ca, null, null);
 	mysqli_real_connect($con, "us-cdbr-iron-east-02.cleardb.net", $cleardb_username, $cleardb_password, $cleardb_db) or die(mysqli_connect_error($con));	
