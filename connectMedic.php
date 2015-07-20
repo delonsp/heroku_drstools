94c69c4bff5264f560d95d278bc08d32acd69c68
Alain Dutra <alain@drsolution.org>
2015-07-20 11:19:37 -0300

    OK

--- connectMedic.php
+++ connectMedic.php
@@ -4,54 +4,56 @@
 // $DB = "drsoluti2";
 
 
-// Dados do banco do heroku
-$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-$cleardb_server = $cleardb_url["host"];
-$cleardb_username = $cleardb_url["user"];
-$cleardb_password = $cleardb_url["pass"];
-$cleardb_db = substr($cleardb_url["path"], 1);
 
-// certificados
-
-// cert_pem
-$cert_pem = getenv("CERT_PEM");
-
-
-//cert_key
-$cert_key = getenv("CERT_KEY");
-
-//cleardb_ca
-
-$cleardb_ca = getenv("CLEARDB_CA");
-
-
-
-
-// Nomes dos campos das tabelas
-$nomeDaReceitaDB = "nomeDaReceita";
-$descricaoDB = "descricao";
-$no = "no";
-$localDB="local";
-$logoDB="logo";
-$nomeDB = "nome";
-$doencaDB = "doenca";
-$man = "man";
-$userId = "user_id";
-$userEmail = "user_email";
-$userPass = "user_password";
-
-// Nomes das tabelas
-$tabelaTISS = "codigosTISS";
-$tabelaReceitas = "bancoDeReceitas";
-$tabelaExames = "relExames";
-$tabelaUser = 'user_system';
 
 
 
 function connect() {
 
-	global $cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db;
-	global $cert_pem, $cert_key, $cleardb_ca;
+	// Dados do banco do heroku
+	$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+	$cleardb_server = $cleardb_url["host"];
+	$cleardb_username = $cleardb_url["user"];
+	$cleardb_password = $cleardb_url["pass"];
+	$cleardb_db = substr($cleardb_url["path"], 1);
+
+	// certificados
+
+	// cert_pem
+	$cert_pem = getenv("CERT_PEM");
+
+
+	//cert_key
+	$cert_key = getenv("CERT_KEY");
+
+	//cleardb_ca
+
+	$cleardb_ca = getenv("CLEARDB_CA");
+
+
+
+
+	// Nomes dos campos das tabelas
+	$nomeDaReceitaDB = "nomeDaReceita";
+	$descricaoDB = "descricao";
+	$no = "no";
+	$localDB="local";
+	$logoDB="logo";
+	$nomeDB = "nome";
+	$doencaDB = "doenca";
+	$man = "man";
+	$userId = "user_id";
+	$userEmail = "user_email";
+	$userPass = "user_password";
+
+	// Nomes das tabelas
+	$tabelaTISS = "codigosTISS";
+	$tabelaReceitas = "bancoDeReceitas";
+	$tabelaExames = "relExames";
+	$tabelaUser = 'user_system';
+
+	// global $cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db;
+	// global $cert_pem, $cert_key, $cleardb_ca;
 	
 	$con = mysqli_init();
 	mysqli_ssl_set($con, $cert_key, $cert_pem, $cleardb_ca, null, null);
