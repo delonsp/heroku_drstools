59e11b7f0de1ba2d54d1d8ea88b9f9593fb480f2
Alain Dutra <alain@drsolution.org>
2015-07-20 11:14:26 -0300

    OK

--- connectMedic.php
+++ connectMedic.php
@@ -55,7 +55,7 @@ function connect() {
 	
 	$con = mysqli_init();
 	mysqli_ssl_set($con, $cert_key, $cert_pem, $cleardb_ca, null, null);
-	mysqli_real_connect($con, "us-cdbr-iron-east-02.cleardb.net", $cleardb_username, $cleardb_password, $cleardb_db) or die(mysqli_connect_error($con));	
+	mysqli_real_connect($con, $cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db) or die(mysqli_connect_error($con));	
 	mysqli_set_charset($con,'utf8');
 	return $con;
 }
