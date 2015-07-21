d0fb931b1669c2647648a0cafcdbd3b1c809013e
Alain Dutra <alain@drsolution.org>
2015-07-21 15:11:28 -0300

    ok

--- header.php
+++ header.php
@@ -23,7 +23,7 @@
     <link rel="stylesheet" type="text/css" href="css/theme.css">
     <link rel="stylesheet" type="text/css" href="css/external-pages.css">
     <link rel="stylesheet" type="text/css" href="css/animate.css">
-    <link href="http://fonts.googleapis.com/css?family=Lato:300,400,700,900,300italic,400italic,700italic,900italic" rel="stylesheet" type='text/css'>
+    <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,900,300italic,400italic,700italic,900italic" rel="stylesheet" type='text/css'>
 </head>
 <body>
 <a href="#" class="scrolltop">
