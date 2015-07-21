6ede660d32d48cc9e03a168887b12f5db5a5cbce
Alain Dutra <alain@drsolution.org>
2015-07-21 11:57:54 -0300

    ok

--- ChecaSessao.php
+++ ChecaSessao.php
@@ -1,10 +1,10 @@
 <?php 
 
-session_start();     
-     
+session_start();
 
 if(isset($_POST['login'], $_POST['pass']) || isset($_COOKIE['login'], $_COOKIE['pass'])) {
-    $con = connect();
+    $con = connect($DB);
+    
     if(isset($_COOKIE['login'], $_COOKIE['pass'])) {
         $email = mysqli_real_escape_string($con, $_COOKIE['login']);
         $pass = mysqli_real_escape_string($con, $_COOKIE['pass']);
@@ -15,15 +15,16 @@ if(isset($_POST['login'], $_POST['pass']) || isset($_COOKIE['login'], $_COOKIE['
     }
 
     $query1 = "SELECT COUNT(`$userId`) FROM $tabelaUser WHERE `$userEmail` = '$email' AND `$userPass` = '$pass'";
-    
+
     $users = mysqli_query($con, $query1);
     $total = my_mysqli_result($users,0); // from connectMedic.php
     
     if ($total ==1) {
         
-        setcookie('login', $_POST['login'], time() + 60*60*24*10);
-        setcookie('pass', $_POST['pass'], time() + 60*60*24*10);
-        $_SESSION['logged_in'] = 1;  
+        setcookie('login', $_POST['login'], time() + 600);
+        setcookie('pass', $_POST['pass'], time() + 600);
+        $_SESSION['logged_in'] = 1;
+    
     }
     
     if(isset($_SESSION['last_ip']) === false) {
@@ -36,6 +37,8 @@ if(isset($_POST['login'], $_POST['pass']) || isset($_COOKIE['login'], $_COOKIE['
         session_unset();
         session_destroy();  
     }
-}
+}     
+     
+
 
  ?>
