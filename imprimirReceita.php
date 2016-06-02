ae36e17fde6a429051e3a3c688c0872862d8ec50
Alain Dutra <alain@drsolution.org>
2016-06-02 09:50:38 -0300

    changed stamp

--- imprimirReceita.php
+++ imprimirReceita.php
@@ -90,7 +90,7 @@
 
         <div id="signature">
             
-                <img src="img/carimbo.png">
+                <img src="img/carimbo.png" height="50" width="100">
                 <hr/>
                 
                 <?php
